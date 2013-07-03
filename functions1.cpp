d6b27e407d472d190be9cea0ce424daa728afa27
ashish786 <ashish.iitianr@gmail.com>
2013-07-03 22:24:17 +0530

    Create functions1.cpp

---
+++ functions1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char a[2][20] = {"ashish", "kumar"};
+  cout<<a[0];
+  cout<<endl;
+  char *b[5] = {"as","dfff"};
+  cout<<b[0];
+   
+   return 0;
+}
